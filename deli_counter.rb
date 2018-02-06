b7eb4bba3fb4ab0288027848f55868a41c4eed6d
Kayvan Vakili <kvakili813@gmail.com>
2018-02-06 02:38:10 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(katz_deli)
   if katz_deli.length==0
     puts "The line is currently empty."
   else
-    puts "The line is currently: #{other_deli}"
+    puts "The line is currently: #{katz_deli}"
   end
 end
 
