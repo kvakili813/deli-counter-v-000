b5ebab6ca9d8702b9a5c11d06ce090aacaf8e2a2
Kayvan Vakili <kvakili813@gmail.com>
2018-02-06 01:55:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli.length==0
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(katz_deli, name)
+  count=1
+  if katz_deli.length > 0
+    name << katz_deli
+  puts "The line is currently: other_deli"
+end
+end
