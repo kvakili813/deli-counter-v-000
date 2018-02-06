1c367a90963e093117bb200001041b149266f50a
Kayvan Vakili <kvakili813@gmail.com>
2018-02-06 05:02:02 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,7 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
-def now_serving(katz_deli)
+def now_serving
   if katz_deli==0
     puts "There is nobody waiting to be served!"
   else
