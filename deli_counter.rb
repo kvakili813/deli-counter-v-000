f47bc680c181237da30860f24bb5c8ca157abe14
Kayvan Vakili <kvakili813@gmail.com>
2018-02-06 02:45:48 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,10 @@ def line(katz_deli)
   count=1
   if katz_deli.length==0
     puts "The line is currently empty."
-  else
+  else 
+    katz_deli.each do |name|  
+      name
+    end
     puts "The line is currently: #{katz_deli}"
   end
 end
